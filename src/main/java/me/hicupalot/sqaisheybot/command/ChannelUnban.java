88228c2e838f7cf2a97b72692c8b0d1396cef0db
hicupalot <hicupalot@gmail.com>
2021-10-14 16:04:27 +0100

    Fixed one small error

--- src/main/java/me/hicupalot/sqaisheybot/command/ChannelUnban.java
+++ src/main/java/me/hicupalot/sqaisheybot/command/ChannelUnban.java
@@ -30,9 +30,11 @@ public class ChannelUnban extends DiscordCommand {
         assert user != null;
         assert guild != null;
         //-------------------------------------------------------------------------------------------//
-        if (user.getRoles().stream().filter(role -> role.getName().equalsIgnoreCase("Helper")).findAny().orElse(null) != null || user.isOwner()
+        if (guild.getId().equals(main.getConfig().getQuacktopiaDiscord())){
+         if (user.getRoles().stream().filter(role -> role.getName().equalsIgnoreCase("Helper")).findAny().orElse(null) != null || user.isOwner()
                 || user.getRoles().stream().filter(role -> role.getId().equalsIgnoreCase("disc-admin")).findAny().orElse(null) != null || user.hasPermission(Permission.ADMINISTRATOR) || user2.isBot()) {
             event.reply("You can't check this user!").setEphemeral(true).queue();
+        }
         } else if (guild.getId().equals(main.getConfig().getSqaisheyDiscord())) {
             if (user.getRoles().stream().filter(role -> role.getId().equalsIgnoreCase("\uD83D\uDC23 | Helper")).findAny().orElse(null) != null || user.isOwner() || user.hasPermission(Permission.ADMINISTRATOR) || user2.isBot()) {
                 event.reply("You can't check this user!").setEphemeral(true).queue();
